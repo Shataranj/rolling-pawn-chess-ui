0678323afbba4d1c30e0289ffd1d8ea3fcf0e5bd
Kanishka <kukshalkanishka@gmail.com>
2020-07-04 13:56:02 +0530

    Ankon/Kaniska Add functionality to download pgn for a game.

--- public/default.js
+++ public/default.js
@@ -3,21 +3,14 @@ let game;
 let socket;
 
 const getCookie = function(name) {
-    // Split cookie string and get all individual name=value pairs in an array
     const cookieArr = document.cookie.split(";");
     
-    // Loop through the array elements
     for(let i = 0; i < cookieArr.length; i++) {
         let cookiePair = cookieArr[i].split("=");
-        
-        /* Removing whitespace at the beginning of the cookie name
-        and compare it with the given string */
         if(name == cookiePair[0].trim()) {
-            // Decode the cookie value and return
             return decodeURIComponent(cookiePair[1]);
         }
     }
-    // Return null if not found
     return null;
 }
 
@@ -50,20 +43,46 @@ const handleMove = function (source, target) {
                 body: JSON.stringify(move)
             }))
 }
+const setupGame = function() {
+    fetch('/config').then(res => res.json()).then(({apiURL}) => {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const currentGameID = urlParams.get('gameId')
 
-window.onload = function () {
-  fetch('/config').then(res => res.json()).then(({ apiURL }) => {
+        socket = io.connect(apiURL);
+        socket.on("move", function (msg) {
+            if (msg.game_id == currentGameID) {
+                game.move(msg);
+                board.position(game.fen());
+            }
+        });
+        initGame();
+    })
+}
+
+const download = function(fileName, content) {
+    const element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,'
+        + encodeURIComponent(content));
+    element.setAttribute('download', fileName);
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}
+
+const downloadPgn = function(){
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
-    const currentGameID = urlParams.get('gameId')
+    const gameId = urlParams.get('gameId');
+    fetch('/config').then(res => res.json())
+        .then(({apiURL}) =>
+            fetch(apiURL + `/pgn?gameId=${gameId}`))
+        .then(res => res.json())
+        .then(({pgn}) => download(`${gameId}.pgn`, pgn))
+}
 
-    socket = io.connect(apiURL);
-    socket.on("move", function (msg) {
-      if (msg.game_id == currentGameID) {
-        game.move(msg);
-        board.position(game.fen());
-      }
-    });
-    initGame();
-  })
+window.onload = function () {
+    let downloadButton = document.getElementById("download-pgn");
+    downloadButton.onclick = downloadPgn;
+    setupGame();
 };
